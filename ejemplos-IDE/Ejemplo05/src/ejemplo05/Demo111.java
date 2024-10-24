75cf72612139ac6c30f1b480bdc0bc139923795a
jhordycamacas <jhordycamacas9@gmail.com>
2024-10-24 10:37:20 -0500

    Cambios 10 am

---
+++ ejemplos-IDE/Ejemplo05/src/ejemplo05/Demo111.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplo05;
+
+/**
+ *
+ * @author utpl
+ */
+public class Demo111 {
+    public static void main(String[] args) {
+        int valor1 = 10;
+        double valor2 = 3.0;
+        int valor3 = 1;
+        
+        System.out.println(valor1 / (valor2 + valor3));
+        
+    }
+}
